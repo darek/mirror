5e60f8ff9c01f0065ec7944a8b55f4eb3c16c520
Darek Zoń <git@darekzon.com>
2014-01-24 23:47:10 +0100

    Inicjalizacja projektu, stworzenie prefaba, podstawowej sceny, skryptu poruszania gościem

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+
+	[HideInInspector]
+	public bool activeCharacter = false;
+
+
+	public float maxSpeed = 5f;
+
+	public bool canDoubleJump = false;
+	public bool canCrawl = false;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate(){
+		float hForce = Input.GetAxis("Horizontal");
+
+		Debug.Log(hForce);
+
+			rigidbody2D.velocity = new Vector2( hForce * maxSpeed, rigidbody2D.velocity.y);
+
+
+
+	}
+	
+
+
+
+}
