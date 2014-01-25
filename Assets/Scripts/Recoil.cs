82e16a50a8ca44980b28c9a705acd2c95a6237a3
Darek Zoń <git@darekzon.com>
2014-01-25 14:05:16 +0100

    skrypt recoil

---
+++ Assets/Scripts/Recoil.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+	
+public class Recoil : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		Debug.Log("Recoil");
+		collider.gameObject.rigidbody2D.velocity = new Vector2(0f,0f);
+		collider.gameObject.rigidbody2D.AddForce((new Vector2(-5000f,300f)));
+	}
+}
