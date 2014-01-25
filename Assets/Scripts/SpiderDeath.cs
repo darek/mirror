b0a1351fddf9f1fff560f22efb928d1c17e2167f
Darek Zoń <git@darekzon.com>
2014-01-26 00:21:59 +0100

    spplashscreeny - animacje - skrypty przelaczajace poziomy

---
+++ Assets/Scripts/SpiderDeath.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpiderDeath : MonoBehaviour {
+
+	// Update is called once per frame
+	void OnTriggerEnter2D () {
+		Application.LoadLevel (1);
+	}
+}
