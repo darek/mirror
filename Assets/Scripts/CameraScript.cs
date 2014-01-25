e27747c2566f548ab36396ff4e625bfac5229454
Bartosz Wilczyński <vilecq@gmail.com>
2014-01-25 16:47:27 +0100

    Pre-pre-pre Alpha rozgrywki

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -3,11 +3,11 @@ using System.Collections;
 
 public class CameraScript : MonoBehaviour {
 
-	public float cameraSpeed;
+	public float cameraSpeed = 5.5f;
 
-	void Update(){
+	void FixedUpdate(){
 		print("Camera moving at " + camera.velocity.magnitude + " m/s");
-		rigidbody2D.velocity = new Vector2 (5.5f,rigidbody2D.velocity.y);
+		rigidbody2D.velocity = new Vector2 (cameraSpeed,0);
 		//rigidbody2D.velocity = new Vector2 (cameraSpeed, 0);
 
 	}
