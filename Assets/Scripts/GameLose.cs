3aacc58859aee5dffdfb4102de64b9463277fd58
Darek Zoń <git@darekzon.com>
2014-01-26 10:20:29 +0100

    wygrywanie gry, przegrywanie gry, particle smierc pajecza

---
+++ Assets/Scripts/GameLose.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLose : MonoBehaviour {
+
+
+	void OnTriggerEnter2D (Collider2D collider) {
+		if(collider.gameObject.tag=="Player"){
+			Application.LoadLevel (1);
+		}
+	}
+}
---
+++ Assets/Scripts/GameWon.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameWon : MonoBehaviour {
+
+	// Update is called once per frame
+	void OnTriggerEnter2D () {
+		Debug.Log("asdasd");
+		Application.LoadLevel (1);
+	}
+}
