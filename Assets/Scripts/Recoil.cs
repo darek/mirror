f835389e5a8b6e318402c3eb99696baf83c02798
Bartosz Wilczyński <vilecq@gmail.com>
2014-01-26 12:59:20 +0100

    Water + hearts

--- Assets/Scripts/Recoil.cs
+++ Assets/Scripts/Recoil.cs
@@ -17,19 +17,23 @@ public class Recoil : MonoBehaviour {
 	
 	void OnTriggerEnter2D(Collider2D collider) {
 		Debug.Log ("recoil");
-		float currentTime = Time.time * 1000;
-		if (!eventHandled && currentTime - exitTime > 100f) {
+		//if (!eventHandled) {
+			
 			if (collider.gameObject.tag == "Player") {
 				collider.gameObject.rigidbody2D.velocity = Vector2.zero;
 				collider.gameObject.rigidbody2D.AddForce ((new Vector2 (recoilForceX, recoilForceY)));
 				collider.gameObject.GetComponent<Animator> ().SetBool ("Recoil", true);
 				if (collider.gameObject.GetComponent<Character> ().activePlayer) {
 					AudioSource.PlayClipAtPoint (ouchSound, transform.position);
-					decreaseHeart ();
-					eventHandled = true;
+
+					float currentTime = Time.time * 1000;
+					if(!eventHandled && (currentTime - exitTime > 100f)){
+						eventHandled = true;
+						decreaseHeart ();
+					}
 				}
 			}
-		}
+		//}
 	}
 
 	void OnTriggerExit2D (Collider2D other){
