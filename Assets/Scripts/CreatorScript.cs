01dcfdfdc672094d5dba17c67687d3b253d9fbe9
Michał Sitko <msitko68@gmail.com>
2014-01-26 14:19:38 +0100

    dodanie dziur w trawie

--- Assets/Scripts/CreatorScript.cs
+++ Assets/Scripts/CreatorScript.cs
@@ -6,17 +6,30 @@ public class CreatorScript : MonoBehaviour {
 
 	public GameObject [] patternObjects = new GameObject[] {};
 
+	public int[] indexesToIgnore;
+
+	public GameObject objectForIgnore = null;
+
 	// liczba ile razy trzeba powtorzyc obiekt, zeby zajal cala przestrzen, domyslnie 16
 	public int objectNTimesRepeated = 16;
+
+	private int counterForIgnore = 0;
 
 	void Start(){
 		GameObject current = theLastOne;
 
 		CreateMirrored (current);
 
+		counterForIgnore = 1;
+
+		GameObject tmp;
+
 		for(int i=0; i<objectNTimesRepeated; ++i){
-			current = CreateNextHorizontal(current);
-			CreatorScript.CreateMirrored(current);
+			tmp = CreateNextHorizontal(current);
+			if(tmp){
+				current = tmp;
+				CreatorScript.CreateMirrored(current);
+			}
 		}
 
 		theLastOne = current;
@@ -24,7 +37,9 @@ public class CreatorScript : MonoBehaviour {
 
 	private GameObject CreateNextHorizontal(GameObject previous){
 		Vector3 newlyCreatedPosition = new Vector3 (previous.transform.position.x + previous.renderer.bounds.size.x, previous.transform.position.y, previous.transform.position.z);
-		GameObject newlyCreated = cloneGameObject(chooseRandomPatternObject(), newlyCreatedPosition);
+		GameObject newlyCreated = cloneGameObjectNonStatic(chooseRandomPatternObject(), newlyCreatedPosition);
+
+
 		return newlyCreated;
 	}
 
@@ -57,17 +72,32 @@ public class CreatorScript : MonoBehaviour {
 			float paddingX = (theLastOne.renderer.bounds.size.x + newObjectPattern.renderer.bounds.size.x)/2f;
 			Vector3 newlyCreatedPosition = new Vector3(theLastOne.transform.position.x + paddingX, destroyedObjectPosition.y, destroyedObjectPosition.z);
 
-			GameObject newlyCreated = cloneGameObject(newObjectPattern, newlyCreatedPosition);
-
-			CreatorScript.CreateMirrored(newlyCreated);
-
-			theLastOne = newlyCreated;
+			GameObject newlyCreated = cloneGameObjectNonStatic(newObjectPattern, newlyCreatedPosition);
+			if(newlyCreated){
+				CreatorScript.CreateMirrored(newlyCreated);
+				theLastOne = newlyCreated;
+			}
 		}
 	}
 
+	private GameObject cloneGameObjectNonStatic(GameObject gameObject, Vector3 position){
+		GameObject newlyCreated = null;
+		Debug.Log ("grass ignore counter: " + counterForIgnore);
+		if (!Array.Exists(indexesToIgnore, element => element == counterForIgnore)) {
+			newlyCreated = Instantiate (gameObject, position, Quaternion.identity) as GameObject;
+			newlyCreated.GetComponent<BackgroundElementScript> ().canBeDeleted = true;
+		}else{
+			Debug.Log ("zignorowano trawe");
+			newlyCreated = Instantiate (objectForIgnore, position, Quaternion.identity) as GameObject;
+			newlyCreated.GetComponent<BackgroundElementScript> ().canBeDeleted = true;
+		}
+		counterForIgnore += 1;
+		return newlyCreated;
+	}
+
 	private static GameObject cloneGameObject(GameObject gameObject, Vector3 position){
-		GameObject newlyCreated = Instantiate(gameObject, position, Quaternion.identity) as GameObject;
-		newlyCreated.GetComponent<BackgroundElementScript>().canBeDeleted = true;
+			GameObject newlyCreated = Instantiate (gameObject, position, Quaternion.identity) as GameObject;
+			newlyCreated.GetComponent<BackgroundElementScript> ().canBeDeleted = true;
 		return newlyCreated;
 	}
 }
