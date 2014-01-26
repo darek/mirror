74b629fca7da6a4afb9a29cadbadd1902debff0f
Michał Sitko <msitko68@gmail.com>
2014-01-26 11:00:02 +0100

    Dodanie kropki z progressem

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,12 +6,30 @@ public class GameController : MonoBehaviour {
 	public GameObject man;
 	public GameObject woman;
 
+	public GameObject startPoint;
+	public GameObject endPoint;
+
 	private bool isManActive = true;
 	private Matrix4x4 mat;
+
+	private float startPointX;
+	private float wholeDistance;
+	private float cameraWidth;
 
 	// Use this for initialization
 	void Start () {
 		mat = Camera.main.projectionMatrix;
+
+		// inicjalizujemy pola potrzebne do obliczenia poziomu HUD-pointa
+		startPointX = startPoint.transform.position.x;
+		float endPointX = endPoint.transform.position.x;
+		wholeDistance = Mathf.Abs (endPointX - startPointX);
+
+		//http://answers.unity3d.com/questions/230190/how-to-get-the-width-and-height-of-a-orthographic.html#answer-230205
+		Camera cam = Camera.main;
+		float height = 2f * cam.orthographicSize;
+		cameraWidth = height * cam.aspect;
+		Debug.Log ("whole: " + wholeDistance);
 	}
 	
 	// Update is called once per frame
@@ -30,5 +48,16 @@ public class GameController : MonoBehaviour {
 			Camera.main.transform.Rotate(0,0,180);
 		}
 
+		refreshHUDBar ();
+	}
+
+	private void refreshHUDBar(){
+		Camera camera = Camera.main;
+		float distanceDone = Mathf.Abs(camera.transform.position.x - startPointX);
+		float distancePercent = distanceDone / wholeDistance;
+
+		float regardsToCameraCenter = (distancePercent * cameraWidth) - (cameraWidth/2);
+		float newX = camera.transform.position.x + regardsToCameraCenter;
+		transform.position = new Vector3 (newX, transform.position.y, transform.position.z);
 	}
 }
