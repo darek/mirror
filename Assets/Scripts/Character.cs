189cfc8c61f1fc8cd928b45157686f40c9011995
Bartosz Wilczyński <vilecq@gmail.com>
2014-01-26 10:43:14 +0100

    Monster animation

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -12,6 +12,8 @@ public class Character : MonoBehaviour {
 	public GameObject mirror;
 	public Animator animator;
 
+	private bool die = false;
+
 	// Use this for initialization
 	void Awake () {
 		animator = GetComponent<Animator>();
@@ -27,10 +29,19 @@ public class Character : MonoBehaviour {
 
 	void FixedUpdate(){
 		float hForce = Input.GetAxis("Horizontal");
-		if (activePlayer) {
+		if (activePlayer && !die ) {
 			//rigidbody2D.velocity = new Vector2 (hForce * maxSpeed, rigidbody2D.velocity.y);
 			rigidbody2D.velocity = new Vector2 (maxSpeed,rigidbody2D.velocity.y);
 		} 
+
+		if (die && (rigidbody2D.velocity.x >= 0 || rigidbody2D.velocity.y >= 0)) {
+			rigidbody2D.AddForce(new Vector2(-rigidbody2D.velocity.x*4,-rigidbody2D.velocity.y*4));
+		}
+	}
+
+	public void Die(){	
+		die = true;
+		animator.SetTrigger ("Die");
 	}
 
 }
---
+++ Assets/Scripts/MonsterEating.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterEating : MonoBehaviour {
+	
+	public Animator animator;
+	
+	// Use this for initialization
+	void Awake () {
+		animator = GetComponent<Animator>();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void OnTriggerEnter2D(Collider2D collider) {
+		if (collider.gameObject.tag == "Player") {
+			collider.gameObject.GetComponent<Character>().Die();
+			animator.SetTrigger ("Eat");
+		}
+	}
+}
